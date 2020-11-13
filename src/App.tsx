d8f94769085a487451135a851824165f8cdee9b8
Roy Davis <roy.davis@mountainwarehouse.com>
2020-11-13 11:18:59 +0000

    Snake moves & now we have rain

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Canvas } from "./Components/Canvas";
 import './App.css';
+import { randomInt } from 'crypto';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface AppProps {
+
 }
 
+export type BodyCoOrds = {
+  x: number;
+  y: number;
+}
+
+export interface AppState {
+  snakeBody: BodyCoOrds[];
+  snakeLength: number;
+  rain: BodyCoOrds[];
+  newRainInterval: number;
+  timeSinceLastRain: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      snakeBody: [],
+      snakeLength: 100,
+      rain: [],
+      newRainInterval: 500,
+      timeSinceLastRain: 0
+    };
+  }
+
+  update = () => {
+    let { timeSinceLastRain } = this.state;
+    const { rain } = this.state;
+
+    timeSinceLastRain--;
+
+    if (rain.length > 0) rain.forEach(x => x.y++);
+
+    const newRain = rain.filter(x => x.y < 1000);
+
+    if (timeSinceLastRain < 0) {
+      const rx = Math.round(Math.random() * 100);
+      rain.push({ x: rx, y: 0 });
+      timeSinceLastRain = 500;
+    }
+
+    this.setState({ rain: newRain, timeSinceLastRain });
+  }
+
+  draw = () => {
+    //this.state.snakeBody.forEach(x => console.log(x.x, x.y));
+  }
+
+  setStateWrapper = (state: AppState) => {
+    this.setState(state);
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.update();
+      //this.draw();
+    }, 1000 / 60)
+  }
+  render() {
+    return (<div className="App">
+      <Canvas state={this.state} setState={this.setStateWrapper}></Canvas>
+    </div>);
+  }
+}
+ 
 export default App;
+export type SetStateHandler = (state: AppState) => void;
---
+++ src/Components/Canvas.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { AppState, BodyCoOrds, SetStateHandler } from '../App';
+
+export interface CanvasProps {
+    state: AppState;
+    setState: SetStateHandler;
+}
+export const Canvas = ({ state, setState }: CanvasProps) => {
+    const canvasRef = React.useRef<HTMLCanvasElement>(null);
+    React.useEffect(() => {
+        const context = canvasRef.current && canvasRef.current.getContext("2d");
+        if (context) {
+            // do our draw here, incoming from props
+            context.fillStyle = "white";
+            context.fillRect(0, 0, canvasRef.current!.width, canvasRef.current!.height);
+            //context.beginPath();
+            state.snakeBody.forEach(x => {
+                context.beginPath();
+                context.arc(x.x, x.y, 1, 0, 2 * Math.PI);
+                context.fillStyle = "green";
+                context.fill();
+                context.closePath();
+            });
+            state.rain.forEach(x => {
+                context.beginPath();
+                context.arc(x.x, x.y, 1, 0, 2 * Math.PI);
+                context.fillStyle = "red";
+                context.fill();
+                context.closePath();
+            })
+            //context.closePath();
+        }
+    });
+
+    return (<canvas ref={canvasRef} style={{ width: "100%", height: "100%" }} onMouseMove={(e) => mouseMoveHandler(e.clientX, e.clientY, state, setState, canvasRef)}></canvas>)
+}
+
+const mouseMoveHandler = (x: number, y: number, state: AppState, setState: SetStateHandler, canvasRef: React.RefObject<HTMLCanvasElement>) => {
+    if (canvasRef.current) {
+        const rect = canvasRef.current.getBoundingClientRect();
+        const scaleX = canvasRef.current.width / rect.width;
+        const scaleY = canvasRef.current.height / rect.height;
+        const xpos = (x - rect.left) * scaleX;
+        const ypos = (y - rect.top) * scaleY;
+        const pos = { x: xpos, y: ypos } as BodyCoOrds;
+        if (state.snakeBody.length === state.snakeLength) {
+            const body = state.snakeBody.slice(1);
+            body.push(pos);
+            state.snakeBody = body;
+        } else
+            state.snakeBody.push(pos);
+        setState(state);
+    }
+}
